b118aae745ae4b7c5e0645c78af84dbff4f4e6fb
Kendall Pfeifer <kpfeifer@umd.edu>
2023-05-12 20:03:33 -0400

    added basic translation functionality

--- translatorApp.js
+++ translatorApp.js
@@ -15,11 +15,19 @@ app.get('/', function (req, response) {
     response.render('index');
 });
 
+app.get('/translate', function (req, response) {
+    response.render('translate');
+});
+
+app.get('/translationsByEmail', function (req, response) {
+    response.render('translationsByEmail');
+});
+
 runApp();
 
 async function runApp() {
     try {
-        
+
         startServer();
     } finally {
         await client.close();
@@ -41,6 +49,31 @@ async function startServer() {
                 console.log("Shutting down the server");
                 process.exit(0);
             }
+        });
+
+        app.post("/translate", async (request, response) => {
+            let data = {
+                name: request.body.name,
+                email: request.body.email,
+                input_text: request.body.input_text,
+                translation_language: request.body.translation_language,
+                translated_text: ""
+            };
+
+            translation_url =`https://api.funtranslations.com/translate/${data.translation_language}.json`;
+            let result = await fetch(translation_url, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ "text": data.input_text })
+            });
+            
+            let response_json = await result.json();
+            data.translated_text = response_json.contents.translated; 
+            response.render("translated_text", data);
+            await collection.insertOne(data);
         });
 
 
